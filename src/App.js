8fc3ce15ded446331c40eddcc31b241261915888
EPetrineLynghaug <136695793+EPetrineLynghaug@users.noreply.github.com>
2025-02-04 14:26:16 +0100

    .

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router";
 import Home from "./pages/Home/Home";
-import Characters from "./pages/Characters/Characters";
+import Characters from "./pages/Characteters/Characters";
 import CharacterDetails from "./pages/CharacterDetails/CharacterDetails";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
